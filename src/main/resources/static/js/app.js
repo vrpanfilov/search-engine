cac3fb33c487a68cdf66f5293e5f53795bfeefc8
Vladimir Panfilov <vrpanfilov@yandex.ru>
2023-05-17 21:45:03 +0300

    UI implemented.

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,41 +1,81 @@
 'use strict';
 
-const classList = document.getElementById('app-sidepanel').classList;
+const sidepanelClassList = document.getElementById('app-sidepanel').classList;
+
+const dashboardClassList = document.getElementById('dashboard-command').classList;
+const managementClassList = document.getElementById('management-command').classList;
+const searchClassList = document.getElementById('search-command').classList;
 
 function responsiveSidePanel() {
   if (window.innerWidth >= 1200) {
-    classList.remove('sidepanel-hidden');
-    classList.add('sidepanel-visible');
+    sidepanelClassList.remove('sidepanel-hidden');
+    sidepanelClassList.add('sidepanel-visible');
 
   } else {
-    classList.remove('sidepanel-visible');
-    classList.add('sidepanel-hidden');
+    sidepanelClassList.remove('sidepanel-visible');
+    sidepanelClassList.add('sidepanel-hidden');
   }
 }
 
 $(function () {
+
   $(window).on('load', function () {
     console.log('load');
     responsiveSidePanel();
   });
 
-  $(window).on('resize', function () {
+  $(window).resize(function () {
     responsiveSidePanel();
   });
 
-  $('#sidepanel-toggler').on('click', function () {
-    if (classList.contains('sidepanel-visible')) {
-      classList.remove('sidepanel-visible');
-      classList.add('sidepanel-hidden');
+  $('#sidepanel-toggler').click(function () {
+    if (sidepanelClassList.contains('sidepanel-visible')) {
+      sidepanelClassList.remove('sidepanel-visible');
+      sidepanelClassList.add('sidepanel-hidden');
     } else {
-      classList.remove('sidepanel-hidden');
-      classList.add('sidepanel-visible');
+      sidepanelClassList.remove('sidepanel-hidden');
+      sidepanelClassList.add('sidepanel-visible');
     }
   });
 
-  $('#sidepanel-close').on('click', function (e) {
+  $('#sidepanel-close').click(function (e) {
     e.preventDefault();
     $('#sidepanel-toggler').click();
   });
 
+  $('#dashboard-command').click(function (e) {
+    e.preventDefault();
+    $('#up-header').text('Dashboard');
+    $('#management').hide();
+    $('#search').hide();
+    managementClassList.remove('active');
+    searchClassList.remove('active');
+    $('#dashboard').show();
+    dashboardClassList.add('active');
+    responsiveSidePanel();
+  });
+
+  $('#management-command').click(function (e) {
+    e.preventDefault();
+    $('#up-header').text('Management');
+    $('#dashboard').hide();
+    $('#search').hide();
+    dashboardClassList.remove('active');
+    searchClassList.remove('active');
+    $('#management').show();
+    managementClassList.add('active');
+    responsiveSidePanel();
+  });
+
+  $('#search-command').click(function (e) {
+    e.preventDefault();
+    $('#up-header').text('Search');
+    $('#dashboard').hide();
+    $('#management').hide();
+    dashboardClassList.remove('active');
+    managementClassList.remove('active');
+    $('#search').show();
+    searchClassList.add('active');
+    responsiveSidePanel()
+  });
 });
